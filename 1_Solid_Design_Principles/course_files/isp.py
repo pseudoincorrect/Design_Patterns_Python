99d103f1950786bea9aefbdb0dc4aac9faf675fe
Maxime Clement <maxime-clement@live.fr>
2019-04-04 14:52:04 +0300

    chapter 1 done

---
+++ 1_Solid_Design_Principles/course_files/isp.py
@@ -0,0 +1,91 @@
+""" Interface Segragation Principle """
+
+from abc import abstractmethod
+
+
+class Machine:
+    def print2(self, document):
+        raise NotImplementedError()
+
+    def fax(self, document):
+        raise NotImplementedError()
+
+    def scan(self, document):
+        raise NotImplementedError()
+
+
+# ok if you need a multifunction device
+class MultiFunctionPrinter(Machine):
+    def print2(self, document):
+        pass
+
+    def fax(self, document):
+        pass
+
+    def scan(self, document):
+        pass
+
+
+class OldFashionedPrinter(Machine):
+    def print2(self, document):
+        # ok - print2 stuff
+        pass
+
+    def fax(self, document):
+        pass  # do-nothing
+
+    def scan(self, document):
+        """Not supported!"""
+        raise NotImplementedError('Printer cannot scan!')
+
+
+class Printer:
+    @abstractmethod
+    def print2(self, document): pass
+
+
+class Scanner:
+    @abstractmethod
+    def scan(self, document): pass
+
+
+# same for Fax, etc.
+
+class MyPrinter(Printer):
+    def print2(self, document):
+        print(document)
+
+
+class Photocopier(Printer, Scanner):
+    def print2(self, document):
+        print(document)
+
+    def scan(self, document):
+        pass  # something meaningful
+
+
+class MultiFunctionDevice(Printer, Scanner):  # , Fax, etc
+    @abstractmethod
+    def print2(self, document):
+        pass
+
+    @abstractmethod
+    def scan(self, document):
+        pass
+
+
+class MultiFunctionMachine(MultiFunctionDevice):
+    def __init__(self, printer, scanner):
+        self.printer = printer
+        self.scanner = scanner
+
+    def print2(self, document):
+        self.printer.print2(document)
+
+    def scan(self, document):
+        self.scanner.scan(document)
+
+
+printer = OldFashionedPrinter()
+printer.fax(123)  # nothing happens
+printer.scan(123)  # oops!
