2d1c805c4933a43df9633bfec8e38a2d1f25a6f3
Maxime Clement <maxime-clement@live.fr>
2019-05-16 14:04:25 +0300

    summary added SOLID

--- 1_Solid_Design_Principles/course_files/lsp.py
+++ 1_Solid_Design_Principles/course_files/lsp.py
@@ -1,5 +1,6 @@
 """ Liskov Substitution Principle """
 
+
 class Rectangle:
     def __init__(self, width, height):
         self._height = height
@@ -28,6 +29,7 @@ class Rectangle:
     def height(self, value):
         self._height = value
 
+
 class Square(Rectangle):
     def __init__(self, size):
         Rectangle.__init__(self, size, size)
@@ -48,8 +50,9 @@ def use_it(rc):
     print(f'Expected an area of {expected}, got {rc.area}')
 
 
-rc = Rectangle(2, 3)
-use_it(rc)
+if __name__ == "__main__":
+    rc = Rectangle(2, 3)
+    use_it(rc)
 
-sq = Square(5)
-use_it(sq)
+    sq = Square(5)
+    use_it(sq)
